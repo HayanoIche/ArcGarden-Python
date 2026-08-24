92a5f6d75b73e57897e48a978e3c6fb280cb0fda
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-08-24 15:22:40 -0300

    feat: logo do arcgarden e créditos
    Co-Authored-By: Matheus Pereira Vidal <matiaswilson0717@gmail.com>

--- main.py
+++ main.py
@@ -16,19 +16,47 @@ import time
 # Biblioteca de Desenhos no terminal
 import desenho
 
+# -------------------- FUNÇÕES DO PROGRAMA --------------------
+
+def desenhar_logo() -> None:
+    print("""     _-|                                                
+    |_-|                 ____               _           
+    / \   _ __ ___     / ___| __ _ _ __ __| | ___ _ __  
+   / _ \ | '__/ __|   | |  _ / _` | '__/ _` |/ _ \ '_ \ 
+  / ___ \| | | (__    | |_| | (_| | | | (_| |  __/ | | |
+ /_/   \_\_|  \___|    \____|\__,_|_|  \__,_|\___|_| |_|
+ ------------------   ---------------------------------- 
+                                                - ARCEUS""")
+
 # -------------------- PROGRAMA PRINCIPAL --------------------
 
-rodando = True
-navegacao_atual = "inicio"
+# ----- VARIÁVEIS DO PROGRAMA -----
+
+rodando = True              # Se o programa ainda está rodando
+navegacao_atual = "inicio"  # Em qual etapa do programa estamos
+
+# ----- VARIÁVEIS DO JOGO -----
+
+plantas = list()    # Lista de todas as plantas
+missoes = list()    # Lista de todas as missões
+
+jogador = {
+    "username": "",
+    "nome jardim": "",
+    "arc score": 0,
+    "plantas": list()
+}
 
 while (rodando == True):
     # ----- INICIO DO PROGRAMA -----
     if (navegacao_atual == "inicio"):
         # ----- DESENHANDO AS OPÇÕES -----
         desenho.limpar()
-        desenho.titulo("ARC GARDEN", separado=True)
+        desenho.linha()
+        desenhar_logo()
+        desenho.linha()
         desenho.menu(
-            ["Jogar","Gerenciar Sistema"],
+            ["Jogar","Gerenciar Sistema", "Créditos"],
             descricao="Escolha o que deseja fazer:",
             sair=True
         )
@@ -51,6 +79,35 @@ while (rodando == True):
             case "2":
                 navegacao_atual = "gerenciando sistema"
                 continue
+
+            case "3":
+                desenho.limpar()
+                desenho.titulo("CRÉDITOS", separado=True)
+                print("""
+Conheça o time ARCEUS:
+
+IGOR MATEUS DA SILVA
+
+  - Github: https://github.com/HayanoIche
+  - Linkedin: https://www.linkedin.com/in/igor-mateus-da-silva-4b05013ba/
+
+MARCELA BATISTA TEIXEIRA
+
+  - Github: https://github.com/wonbindasilva
+  - Linkedin: https://www.linkedin.com/in/marcelabteixeira/
+
+MATHEUS PEREIRA VIDAL
+
+  - Github: https://github.com/pereirinh44
+  - Linkedin: https://linkedin.com/in/matheus-pereira-8700893b3
+
+FRANCO JARED MARQUINA
+
+  - Github: https://github.com/Fmarquina
+  - Linkedin: https://www.linkedin.com/in/franco-jared-marquina-blas-b67851303/
+
+""")
+                desenho.espera_entrada()
             
             case _:
                 print("ERRO! Opção inválida!")
@@ -59,7 +116,6 @@ while (rodando == True):
     # ----- JOGANDO O ARC GARDEN -----
     elif (navegacao_atual == "jogando"):
         desenho.limpar()
-        print("JOGAR: A SER IMPLEMENTADO!")
         desenho.espera_entrada()
         
     # ----- GERENCIANDO O SISTEMA -----
