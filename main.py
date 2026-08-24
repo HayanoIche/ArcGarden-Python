e32ae9919aad40828e9b855f12d97ae397d0c711
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-08-24 14:07:38 -0300

    feat: navegação do programa
    Co-Authored-By: Matheus Pereira Vidal <matiaswilson0717@gmail.com>

--- main.py
+++ main.py
@@ -19,8 +19,51 @@ import desenho
 # -------------------- PROGRAMA PRINCIPAL --------------------
 
 rodando = True
+navegacao_atual = "inicio"
 
 while (rodando == True):
-    desenho.limpar()
-    desenho.titulo("ARC GARDEN", separado=True)
-    desenho.espera_entrada()
+    # ----- INICIO DO PROGRAMA -----
+    if (navegacao_atual == "inicio"):
+        # ----- DESENHANDO AS OPÇÕES -----
+        desenho.limpar()
+        desenho.titulo("ARC GARDEN", separado=True)
+        desenho.menu(
+            ["Jogar","Gerenciar Sistema"],
+            descricao="Escolha o que deseja fazer:",
+            sair=True
+        )
+        desenho.linha()
+        print()
+        
+        # ----- ESCOLHENDO -----
+        escolha = input("Escolha: ")
+        match(escolha):
+            case "0":
+                desenho.limpar()
+                rodando = False
+                print("\n\nArcGarden - feito por ARCEUS...")
+                print("Programa fechado!\n\n")
+
+            case "1":
+                navegacao_atual = "jogando"
+                continue
+            
+            case "2":
+                navegacao_atual = "gerenciando sistema"
+                continue
+            
+            case _:
+                print("ERRO! Opção inválida!")
+                desenho.espera_entrada()
+
+    # ----- JOGANDO O ARC GARDEN -----
+    elif (navegacao_atual == "jogando"):
+        desenho.limpar()
+        print("JOGAR: A SER IMPLEMENTADO!")
+        desenho.espera_entrada()
+        
+    # ----- GERENCIANDO O SISTEMA -----
+    elif (navegacao_atual == "gerenciando sistema"):
+        desenho.limpar()
+        print("GERENCIAR: A SER IMPLEMENTADO!")
+        desenho.espera_entrada()
